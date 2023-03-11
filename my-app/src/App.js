5f56930d77a98269c5bfde891da98f407ed024bc
Milan Pultar <milan.pultar@quantasoft.com>
2023-03-12 00:42:46 +0100

    fast push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import logo from './profile.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    {/*  <header className="App-header">*/}
-    {/*    <img src={logo} className="App-logo" alt="logo" />*/}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
     {/*    <p>*/}
     {/*      Edit <code>src/App.js</code> and save to reload.*/}
     {/*    </p>*/}
@@ -17,7 +18,7 @@ function App() {
     {/*    >*/}
     {/*      Learn React*/}
     {/*    </a>*/}
-    {/*  </header>*/}
+      </header>
       <p>Hello World</p>
     </div>
   );
